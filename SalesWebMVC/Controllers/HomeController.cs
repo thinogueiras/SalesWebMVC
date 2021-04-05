326c5d38882e9a14eb1d7a8798de682e2d7cb565
thinogueiras <thiagonogueira@hotmail.com.br>
2021-04-05 19:17:25 -0300

    First controller and Razor pages tests

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course.";
+            ViewData["Student"] = "Thiago Nogueira dos Santos.";
 
             return View();
         }
